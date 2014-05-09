d8ffeb23a2a984754d750c38fa243502c65fcc96
Nilo Lo <buddytimi@hotmail.com>
2014-05-09 06:33:05 -0700

    Plotting code update

--- plot1.R
+++ plot1.R
@@ -1,5 +1,5 @@
 library(lubridate)
-data <- read.table("household_power_consumption.txt", sep=";", header=T)
+data <- read.table("household_power_consumption.txt", sep=";", header=T, stringsAsFactors=F)
 data <- data[data$Date=="1/2/2007" | data$Date=="2/2/2007",]
 data[,1:2] <- lapply(1:2, function(x) data[,x] <- as.character(data[,x]))
 data[,3:9] <- lapply(3:9, function(x) data[,x] <- as.numeric(data[,x]))
@@ -7,8 +7,6 @@ data[,10] <- dmy_hms(paste(data[,1], data[,2]))
 names(data)[10] <- "DateTime"
 
 #1
-windows(480,480)
-hist(data$Global_active_power/500, col="orangered2", xlab="Global Active Power (kilowatts)", main="Global Active Power")
-dev.copy(png, file="plot1.png")
+png("plot1.png", 480,480)
+hist(data$Global_active_power, col="orangered2", xlab="Global Active Power (kilowatts)", main="Global Active Power")
 dev.off()
-
--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 library(lubridate)
-data <- read.table("household_power_consumption.txt", sep=";", header=T)
+data <- read.table("household_power_consumption.txt", sep=";", header=T, stringsAsFactors=F)
 data <- data[data$Date=="1/2/2007" | data$Date=="2/2/2007",]
 data[,1:2] <- lapply(1:2, function(x) data[,x] <- as.character(data[,x]))
 data[,3:9] <- lapply(3:9, function(x) data[,x] <- as.numeric(data[,x]))
@@ -7,10 +7,9 @@ data[,10] <- dmy_hms(paste(data[,1], data[,2]))
 names(data)[10] <- "DateTime"
 
 #3
-windows(480,480)
+png("plot3.png", 480,480)
 plot(data$DateTime, data$Sub_metering_1, type="l", ylab="Energy sub metering", xlab="", col="black")
 points(data$DateTime, data$Sub_metering_2, type="l", col="red")
 points(data$DateTime, data$Sub_metering_3, type="l", col="blue")
 legend("topright",lty=1, col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"))
-dev.copy(png, file="plot3.png")
 dev.off()
